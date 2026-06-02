fa896800c24824fe734cca2747382b30a503e60a
Wagner Loch <wagner_loch@hotmail.com>
2026-06-02 11:01:55 -0300

    Atualização

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
+    res.send('Atualização automática através de CI/CD!');
 });
 
 app.listen(port, () => {
